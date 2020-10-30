5e63b14c5ee9722f8cc7020c9b4d924493dc5dc3
haseebcgt <abdul.haseeb@cgtindia.in>
2020-10-30 19:09:50 +0530

    Added databinding in fragment_quiz_start.xml

--- app/src/main/java/com/pensource/ignouspaq/ui/quiz/quiz/QuizQuestionsFragment.kt
+++ app/src/main/java/com/pensource/ignouspaq/ui/quiz/quiz/QuizQuestionsFragment.kt
@@ -5,15 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.pensource.ignouspaq.R
+import com.pensource.ignouspaq.databinding.FragmentQuizStartBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class QuizQuestionsFragment : Fragment() {
+
+    private lateinit var binding: FragmentQuizStartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz_questions, container, false)
+        binding = FragmentQuizStartBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = this@QuizQuestionsFragment
+        }
+        return binding.root
     }
 }
