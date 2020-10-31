f5778cbe064712f9f5b0b0f0e16042270118f8e2
haseebcgt <abdul.haseeb@cgtindia.in>
2020-10-31 12:46:50 +0530

    Added QuizQuestionViewModel.kt

---
+++ app/src/main/java/com/pensource/ignouspaq/ui/quiz/quiz/QuizQuestionViewModel.kt
@@ -0,0 +1,6 @@
+package com.pensource.ignouspaq.ui.quiz.quiz
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class QuizQuestionViewModel @ViewModelInject constructor() : ViewModel()
