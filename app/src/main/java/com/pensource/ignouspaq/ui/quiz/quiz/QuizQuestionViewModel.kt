67b1b109a658e6fcec4e9ecc9f9855b9d1315f0d
haseebcgt <abdul.haseeb@cgtindia.in>
2020-10-31 14:09:31 +0530

    Added a submit button

--- app/src/main/java/com/pensource/ignouspaq/ui/quiz/quiz/QuizQuestionViewModel.kt
+++ app/src/main/java/com/pensource/ignouspaq/ui/quiz/quiz/QuizQuestionViewModel.kt
@@ -1,6 +1,23 @@
 package com.pensource.ignouspaq.ui.quiz.quiz
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.pensource.ignouspaq.data.model.QuizQuestion
 
-class QuizQuestionViewModel @ViewModelInject constructor() : ViewModel()
+class QuizQuestionViewModel @ViewModelInject constructor() : ViewModel() {
+
+    private val _question = MutableLiveData<List<QuizQuestion>>()
+    val question: LiveData<List<QuizQuestion>> = _question
+
+    private val _currentQuestion = MutableLiveData<QuizQuestion>()
+    val currentQuestion: LiveData<QuizQuestion> = _currentQuestion
+
+    private val _questionNumber = MutableLiveData<Int>()
+    val questionNumber: LiveData<Int> = _questionNumber
+
+    fun loadCurrentQuestion(position: Int) {
+        _currentQuestion.value = question.value?.get(position)
+    }
+}
--- app/src/main/java/com/pensource/ignouspaq/ui/quiz/quiz/QuizQuestionsFragment.kt
+++ app/src/main/java/com/pensource/ignouspaq/ui/quiz/quiz/QuizQuestionsFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.pensource.ignouspaq.databinding.FragmentQuizStartBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class QuizQuestionsFragment : Fragment() {
+
+    private val viewModel: QuizQuestionViewModel by viewModels()
 
     private lateinit var binding: FragmentQuizStartBinding
 
@@ -23,4 +27,11 @@ class QuizQuestionsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.questionNumber.observe(viewLifecycleOwner, Observer { position ->
+            viewModel.loadCurrentQuestion(position)
+        })
+    }
 }
